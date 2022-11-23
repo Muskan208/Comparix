d9a4e24d9e805e99660bdd2085c9efab1854e694
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-11-24 01:40:08 +0530

    Graphjs added Analytics page added {Fake data}

--- src/Screens/Scrape.jsx
+++ src/Screens/Scrape.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Graph } from '../Components/Graph';
 
 const Scrape = () => {
+  const categories=["All","Best Seller","T.V","Electronics","Fashion","Mobiles","Bean Bag","Furniture","Tablet","Books","Movie Ticket","Groceries","Home Furnishing","Jewely","Footwear & Shoes","Clothing & Apparel"];
   return (
-    <div>Scrape</div>
+    <div className="analytics">
+      <div className="heading">
+        <h2>Hi! User &#128075;</h2>
+        <p>Choose from handpicked Bestseller categories</p>
+      </div>
+      <div className="mainsection">
+        <div className="searchbar">
+          <p>Search product here for comparison!</p>
+          <input type="text" placeholder='Search'/>
+          {/* <i class="fa fa-search"></i> */}
+        </div>
+        <div className="categories">
+          {categories.map((category)=>
+          <button>
+            {category}
+          </button>
+          )}
+        </div>
+        <div className="Graph">
+          <p>Most Selling Products</p>
+          <Graph/>
+        </div>
+      </div>
+    </div>
   )
 }
 
